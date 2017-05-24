1744464039de6307043f3646c34fe30d4ba185d2
mebaBety <edenzzzmelaku@gmail.com>
2017-05-24 00:35:08 -0700

    eden made changes

---
+++ cloned/src/cloned/Cloned.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cloned;
+
+/**
+ *
+ * @author MEBA
+ */
+public class Cloned {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("the name of the thrread is");
+        
+    }
+    
+}
